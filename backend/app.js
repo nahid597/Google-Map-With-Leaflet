4027fa87743c65f849b3cf2c6b1d6d4c2eb31623
Khairul Islam <45447261+beta-ori@users.noreply.github.com>
2019-05-21 20:54:59 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const workers = require('./routers/workers');
+const users = require('./routers/users')
 
+router.use('/admin/workers', workers);
+router.use('/admin/users', users);
+
+module.exports = router;
