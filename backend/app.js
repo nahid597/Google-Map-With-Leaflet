9c6ab30d350ca9423a2bdb02a00abf5deb4d1fbe
Khairul Islam <45447261+beta-ori@users.noreply.github.com>
2019-05-21 20:54:18 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const bodyParser = require('body-parser'); 
-const mongoose = require('mongoose');
-const workers = require('./routers/workers');
-const users = require('./routers/users');
- const cors = require('cors');
 
-const app = express();
